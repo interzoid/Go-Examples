13c35004e5e5439e0eaebe13d0e7ae29f0306fbc
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-08-23 13:41:14 -0700

    Create GetCurrencyRate.go
    This API retrieves the latest currency exchange rate, provided in US Dollars, for the given three-letter international currency code. These currency rates are compiled from many global sources and are updated several times per day.

---
+++ GetCurrencyRate.go
@@ -0,0 +1,41 @@
+// Get API key at www.interzoid.com/register
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+)
+
+type Payload struct {
+	Symbol   string
+	Name     string
+	Country  string
+	Rate     string
+	Code     string
+	Credits  int
+}
+
+func main() {
+    // Create the struct variable to store the decoded JSON response
+    thePayload := Payload{}
+
+    // Call the API
+    // Can error check here too replacing underscore with 'err'
+    response, _ := http.Get("https://api.interzoid.com/getcurrencyrate?license=
+                YOURAPILICENSEKEY&symbol=GBP")
+
+    if response.StatusCode != 200 {
+        // Report any HTTP Errors
+        fmt.Println("Error: ", response.Status)
+
+    } else {
+        // Decode the JSON and print a comma-delimited response,
+        // one of many ways to format the response
+        _ = json.NewDecoder(response.Body).Decode(&thePayload)
+
+        fmt.Println(thePayload.Symbol + "," + thePayload.Name +
+		"," + thePayload.Country +
+		"," + thePayload.Rate)
+    }
+}
