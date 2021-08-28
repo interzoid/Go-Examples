7bfada945b0040e5a8a4b60d5879b916adb70457
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-08-28 13:07:30 -0700

    Create GetCryptoPrice.go

---
+++ GetCryptoPrice.go
@@ -0,0 +1,38 @@
+// Get API key at www.interzoid.com/register
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Payload struct {
+	Symbol   string
+	Name     string
+	Price    string
+	Code     string
+	Credits  int
+}
+
+func main() {
+	// Create the struct variable to store the decoded JSON response
+	thePayload := Payload{}
+
+	// Call the API
+	// Can error check here too replacing underscore with 'err'
+	response, _ := http.Get("https://api.interzoid.com/getcryptoprice?license=YOURAPIKEY&symbol=ETH")
+
+	if response.StatusCode != 200 {
+		// Report any HTTP Errors
+		fmt.Println("Error: ", response.Status)
+
+	} else {
+		// Decode the JSON and print a comma-delimited response,
+		// one of many ways to format the response
+		_ = json.NewDecoder(response.Body).Decode(&thePayload)
+
+		fmt.Println(thePayload.Symbol + "," + thePayload.Name +
+			"," + thePayload.Price)
+	}
+}
